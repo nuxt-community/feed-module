a82230d9f0ace272a6bf5279fe1c3adf4059115d
Maciej Pędzich <dnalop.me@gmail.com>
2022-02-01 21:36:34 +0100

    Create documentation website

---
+++ docs/docus.config.ts
@@ -0,0 +1,25 @@
+export default {
+  title: '@nuxt-modules/feed',
+  url: 'https://feed-nm.netlify.app',
+  theme: {
+    colors: {
+      primary: '#F78422',
+      prism: {
+        background: '#F4F4F5 dark:#1F2937'
+      }
+    },
+    header: {
+      title: false,
+      logo: {
+        light: '/logo.svg',
+        dark: '/logo.svg'
+      }
+    }
+  },
+  twitter: '@MaciejPedzich',
+  github: {
+    repo: 'nuxt-modules/feed',
+    branch: 'master',
+    releases: true
+  }
+};

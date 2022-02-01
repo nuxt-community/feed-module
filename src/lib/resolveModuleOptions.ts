0ea55df3c990d92e47c6c241529c7b13b36f5104
Maciej Pędzich <dnalop.me@gmail.com>
2022-02-01 10:36:23 +0100

    Remove redundant object resolve condition

--- src/lib/resolveModuleOptions.ts
+++ src/lib/resolveModuleOptions.ts
@@ -5,12 +5,6 @@ export async function resolveModuleOptions(
 ) {
   if (typeof options === 'function') {
     return await options();
-  } else if (
-    typeof options === 'object' &&
-    !Array.isArray(options) &&
-    options !== null
-  ) {
-    return Object.values<FeedConfig>(options);
   } else if (Array.isArray(options)) {
     return options;
   } else {
